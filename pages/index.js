07170c29b9dfba8399fa753fe32bc938547e160b
Andreas Oberdammer <andreas.oberdammer+coding@gmail.com>
2023-03-01 21:58:23 +0100

    Updated Content and fixed text alignment

--- pages/index.js
+++ pages/index.js
@@ -94,7 +94,7 @@ export default function Home() {
           <Socials className="mt-2 laptop:mt-5" />
         </div>
         <div className="mt-10 laptop:mt-30 p-2 laptop:p-0" ref={workRef}>
-          <h1 className="text-2xl text-bold">Work.</h1>
+          <h1 className="text-4xl text-bold">Work</h1>
 
           <div className="mt-5 laptop:mt-10 grid grid-cols-1 tablet:grid-cols-2 gap-4">
             {data.projects.map((project) => (
@@ -108,19 +108,6 @@ export default function Home() {
             ))}
           </div>
         </div>
-
-        <div className="mt-10 laptop:mt-30 p-2 laptop:p-0">
-          <h1 className="tablet:m-10 text-2xl text-bold">Services.</h1>
-          <div className="mt-5 tablet:m-10 grid grid-cols-1 laptop:grid-cols-2 gap-6">
-            {data.services.map((service, index) => (
-              <ServiceCard
-                key={index}
-                name={service.title}
-                description={service.description}
-              />
-            ))}
-          </div>
-        </div>
         {/* This button should not go into production */}
         {process.env.NODE_ENV === "development" && (
           <div className="fixed bottom-5 right-5">
@@ -130,12 +117,22 @@ export default function Home() {
           </div>
         )}
         <div className="mt-10 laptop:mt-40 p-2 laptop:p-0" ref={aboutRef}>
-          <h1 className="tablet:m-10 text-2xl text-bold">About.</h1>
-          <p className="tablet:m-10 mt-2 text-xl laptop:text-3xl w-full laptop:w-3/5">
-            {data.aboutpara}
-          </p>
+          <h1 className="text-4xl text-bold">About</h1>
+          <div className="items-center mt-5 laptop:mt-10 grid grid-cols-1 laptop:grid-cols-1 gap-4">
+              <div className="flex place-content-center">
+                <img
+                    alt={'Profil picture'}
+                    className="h-1/3 w-1/3 mb-10 object-cover"
+                    src={'https://public-files.gumroad.com/ad03mb8wqboyzmratb6lm6pg2xui'}
+                    style={{borderRadius: 100000/ 2}} 
+                >
+                </img>
+              </div>
+            <p className="text-3xl text-justify">
+              {data.aboutpara}
+            </p>
+          </div>
         </div>
-        <Footer />
       </div>
     </div>
   );
